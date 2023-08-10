0729ca021f6dfab68ac40f16dc3b264f0bf595e0
tuankiet1999 <46787422+tuankiet1999@users.noreply.github.com>
2023-08-10 20:41:35 +0700

    Update get_recommended_filters.sh
    adding news filter domain list

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -12,6 +12,10 @@ urls=(
   https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt
   https://adaway.org/hosts.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+  #news
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/multi.txt
 )
 
 # loop through the urls and download each file with curl
